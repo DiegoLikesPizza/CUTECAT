3752cd19bf61b42a7762c0887a1435a655cb3c10
DiegoLikesPizza <127695789+DiegoLikesPizza@users.noreply.github.com>
2025-05-15 09:55:28 +0200

    feat: drive w/o steer feat: drive w/ steer feet: shoot

--- src/main/java/com/CUTECAT/modes/modebase.java
+++ src/main/java/com/CUTECAT/modes/modebase.java
@@ -1,6 +1,14 @@
 package com.CUTECAT.modes;
 
-public abstract class modebase {
+import com.CUTECAT.diegoutil.DiegoArdUtils;
+
+public abstract class modebase extends DiegoArdUtils {
+
+    private final double WEIGHT = 0.0002;
+
+    private int ammoLeft;
+    private int preferredKeySpeed;
+
 
     // Methoden zum Eingeben des Ziels
     public abstract void setTargetDistance(int newDistance);
@@ -9,6 +17,27 @@ public abstract class modebase {
 
     // Methode zum Eingeben der verbleibenden Munition
     public abstract void setRemainingAmmo(int RemainingAmmo);
+
+
+    // manual / semi / auto
+
+    public void driveWOSteer(boolean forward, int speed) throws Exception {
+        setFront(forward);
+        setSpeed(speed);
+        setDriveYaw(0);
+    }
+
+    public void driveWSteer(boolean forward, int speed, int yaw) throws Exception {
+        setFront(forward);
+        setSpeed(speed);
+        setDriveYaw(yaw);
+    }
+
+    public void shootOnce(){
+        shoot();
+    }
+
+    // semi / auto
 
 
 }
