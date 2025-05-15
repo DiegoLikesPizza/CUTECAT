478330cd0a7b283628b0c8c771671925e5d36338
DowntimeTM <diego.goettler6@gmail.com>
2025-05-15 09:53:55 +0200

    feat: func to calc pitch for distance

--- src/main/java/com/CUTECAT/diegoutil/DiegoPhysicsUtils.java
+++ src/main/java/com/CUTECAT/diegoutil/DiegoPhysicsUtils.java
@@ -7,4 +7,19 @@ public class DiegoPhysicsUtils {
     public static double getGravitationalPull(double weight) {
         return  weight * GRAVITATION;
     }
+
+    public static double[] calculateThrowFunction(double distance, double v0) {
+        double sin2Theta = (distance * GRAVITATION) / (v0 * v0);
+
+        if (sin2Theta < -1 || sin2Theta > 1) {
+            return null;
+        }
+
+        double twoTheta = Math.asin(sin2Theta);
+        double theta1 = Math.toDegrees(twoTheta) / 2;
+
+        double theta2 = 90.0 - theta1;
+
+        return new double[]{theta1, theta2};
+    }
 }
