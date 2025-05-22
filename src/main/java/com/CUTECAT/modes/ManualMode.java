b52cd707eda0e65b45a2b708595dccd5baaafe49
DiegoLikesPizza <127695789+DiegoLikesPizza@users.noreply.github.com>
2025-05-22 11:52:51 +0200

    Add movement and shooting capabilities to ManualMode
    Implemented MovementCapable and ShootingCapable interfaces in ManualMode. Added methods for movement, shooting, and capability checks, enhancing functionality and modularity.

--- src/main/java/com/CUTECAT/modes/ManualMode.java
+++ src/main/java/com/CUTECAT/modes/ManualMode.java
@@ -1,14 +1,36 @@
+// ManualMode.java
 package com.CUTECAT.modes;
 
-import com.CUTECAT.diegoutil.*;
+import com.CUTECAT.modes.capabilities.*;
 
-public class ManualMode extends modebase{
-
-    public ManualMode(){
-        super();
+public class ManualMode extends modebase implements MovementCapable, ShootingCapable {
+    @Override
+    public void move(int speed, int direction) {
+        try {
+            driveWSteer(true, speed, direction);
+        } catch (Exception e) {
+            // Handle exception
+        }
     }
 
-    DiegoArdUtils test = new DiegoArdUtils();
+    @Override
+    public void shoot() {
+        shootOnce();
+    }
+
+    @Override
+    public boolean canShoot() {
+        return getAmmoCount() > 0;
+    }
 
 
+    @Override
+    protected void handleTargeting() {
+
+    }
+
+    @Override
+    protected void handleMovement() {
+
+    }
 }
