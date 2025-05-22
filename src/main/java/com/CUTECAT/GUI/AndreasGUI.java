71da4819b42919f6a9d627121467a0d384a1be2b
mayera10 <andreasmayer1003@gmail.com>
2025-05-22 10:14:27 +0200

    Refactor: Widgets to AndreasGUI
    feat: Hauptfenster
    feat: Modi Wahl
    
    feat: Button styling
    feat: button action (noch nicht fertig)
    
    feat: Modus 1
    feat: Modus 1 angefangen
    feat: Anordnung Buttons, etc.
    
    feat: steuerung
    feat: Modus 2 (Halbautomatischer Modus)
    feat: Styling von Headline und Button

--- src/main/java/com/CUTECAT/GUI/AndreasGUI.java
+++ src/main/java/com/CUTECAT/GUI/AndreasGUI.java
@@ -1,11 +1,9 @@
 package com.CUTECAT.GUI;
 
 import com.CUTECAT.app.CUTECAT;
-import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
-import javafx.event.*;
 import javafx.scene.layout.Pane;
 
 ;
@@ -19,7 +17,7 @@ public class AndreasGUI {
         VBox HeadlControls = createSection("Headline links");
 
         Label Label1a = new Label("   CUTECAT");
-        Label1a.getStyleClass().add("styled-text-field");
+        Label1a.getStyleClass().add("styled-titel");
         Label1a.setMinWidth(1300);
         Label1a.setMaxWidth(1300);
         Label1a.setFont(new Font("Arial",28));
@@ -34,7 +32,7 @@ public class AndreasGUI {
         VBox HeadrControls = createSection("Headline rechts");
 
         Label Label1b = new Label("Gymnasium Beilngries");
-        Label1b.getStyleClass().add("styled-text-field");
+        Label1b.getStyleClass().add("styled-titel");
         Label1b.setMinWidth(650);
         Label1b.setMaxWidth(650);
         Label1b.setLayoutX(1300);
@@ -46,40 +44,29 @@ public class AndreasGUI {
 
         //Buttons mit verschiedenen Modi
         VBox Modi = createSection("Modi");
-        Button b1 = new Button("Modus 1");
+        Button b1 = new Button("Ferngesteuerter Modus");
         //Style button
-        b1.setStyle("-fx-background-color: #F0F0F0;");
-        b1.setStyle("-fx-border-color: #008000; -fx-border-width: 50px;");
-        //b1.setStyle("-fx-border-style: solid;");
-        b1.setStyle("-fx-text-fill: #0a0000");
-        b1.setStyle("-fx-font-size: 50px;");
+        b1.getStyleClass().add("styled-button-a");
         b1.setPrefSize(600,150);
         b1.setLayoutX(1200);
         b1.setLayoutY(200);
-
+        //Button aktivieren
         b1.setOnAction(e -> CUTECAT.Handlung=true);
 
 
         Button b2 = new Button("Modus 2");
         //Style button
-        b2.setStyle("-fx-background-color: #F0F0F0;");
-        b2.setStyle("-fx-border-color: #008000; -fx-border-width: 50px;");
-        //b2.setStyle("-fx-border-style: solid;");
-        b2.setStyle("-fx-text-fill: #0a0000");
-        b2.setStyle("-fx-font-size: 50px;");
+        b2.getStyleClass().add("styled-button-a");
         b2.setPrefSize(600,150);
         b2.setLayoutX(1200);
         b2.setLayoutY(400);
-        
+        //Button aktivieren
+        b2.setOnAction(e -> CUTECAT.Handlung2=true);
 
 
         Button b3 = new Button("Modus 3");
         //Style button
-        b3.setStyle("-fx-background-color: #F0F0F0;");
-        b3.setStyle("-fx-border-color: #008000; -fx-border-width: 50px;");
-        //b3.setStyle("-fx-border-style: solid;");
-        b3.setStyle("-fx-text-fill: #0a0000");
-        b3.setStyle("-fx-font-size: 50px;");
+        b3.getStyleClass().add("styled-button-a");
         b3.setPrefSize(600,150);
         b3.setLayoutX(1200);
         b3.setLayoutY(600);
@@ -135,7 +122,7 @@ public class AndreasGUI {
         VBox HeadControls2 = createSection("Headline");
 
         Label Label2a = new Label("Ferngesteuerter Modus");
-        Label2a.getStyleClass().add("styled-text-field");
+        Label2a.getStyleClass().add("styled-titel");
         Label2a.setPrefWidth(1950);
         Label2a.setLayoutX(0);
         Label2a.setLayoutY(50);
@@ -147,7 +134,6 @@ public class AndreasGUI {
         //Anzeige von verschiedenen Parametern
 
         //Rotation Turm
-        VBox Parameter = createSection("Parameter");
         Label Label2b = new Label("Rotation Turm");
         Label2b.getStyleClass().add("styled-text-field");
         Label2b.setMinWidth(300);
@@ -195,13 +181,145 @@ public class AndreasGUI {
         Label2f.getStyleClass().add("styled-text-field");
         Label2f.setMinWidth(300);
         Label2f.setMaxWidth(300);
-        Label2f.setFont(new Font("Arial",18));
-        Entfernung.getChildren().addAll(
+        Label2f.setLayoutX(500);
+        Label2f.setLayoutY(450);
+        Label2f.setFont(new Font("Arial",22));
+        content2.getChildren().addAll(
                 Label2f);
 
 
         //Informationen zur steuerung
+        String [][] steuerung ={
+                {"W","Vorwärts"},
+                {"S","Rückwärts"},
+                {"A","Links"},
+                {"D","Rechts"},
+                {"→","Turm rechts"},
+                {"←","Turm links"},
+                {"↑","Rohr neigen"},
+                {"↓","Rohr senken"},
+        };
+        for (int i =0;i<steuerung.length;i++){
+            Label taste = new Label(steuerung[i][0]);
+            taste.getStyleClass().add("styled-text-field");
+            taste.setMinWidth(100);
+            taste.setMaxWidth(100);
+            taste.setLayoutX(100+i*150);
+            taste.setLayoutY(850);
+            taste.setFont(new Font("Arial",18));
 
+            Label funktion = new Label(steuerung[i][1]);
+            funktion.getStyleClass().add("styled-text-field");
+            funktion.setMinWidth(100);
+            funktion.setMaxWidth(100);
+            funktion.setLayoutX(100+i*150);
+            funktion.setLayoutY(900);
+            funktion.setFont(new Font("Arial",14));
+
+            content2.getChildren().addAll(taste, funktion);
+        }
+
+    }
+
+    //Ferngesteuerter Modus
+    public static void addWidgets3(Pane content3){
+        //Headline
+        Label Label3a = new Label("Halbautomatischer Modus");
+        Label3a.getStyleClass().add("styled-titel");
+        Label3a.setPrefWidth(1950);
+        Label3a.setLayoutX(0);
+        Label3a.setLayoutY(50);
+        Label3a.setFont(new Font("Arial",28));
+
+        content3.getChildren().addAll(
+                Label3a);
+
+        //Anzeige von verschiedenen Parametern
+
+        //Rotation Turm
+        Label Label2b = new Label("Rotation Turm");
+        Label2b.getStyleClass().add("styled-text-field");
+        Label2b.setMinWidth(300);
+        Label2b.setMaxWidth(300);
+        Label2b.setLayoutX(50);
+        Label2b.setLayoutY(150);
+        Label2b.setFont(new Font("Arial",22));
+
+
+        //Neigung Rohr
+        Label Label2c = new Label("Neigung Rohr");
+        Label2c.getStyleClass().add("styled-text-field");
+        Label2c.setMinWidth(300);
+        Label2c.setMaxWidth(300);
+        Label2c.setLayoutX(50);
+        Label2c.setLayoutY(250);
+        Label2c.setFont(new Font("Arial",22));
+
+
+        //Munition
+        Label Label2d = new Label("Munition");
+        Label2d.getStyleClass().add("styled-text-field");
+        Label2d.setMinWidth(300);
+        Label2d.setMaxWidth(300);
+        Label2d.setLayoutX(50);
+        Label2d.setLayoutY(350);
+        Label2d.setFont(new Font("Arial",22));
+
+
+        //Gasdruck
+        Label Label2e = new Label("Gasdruck");
+        Label2e.getStyleClass().add("styled-text-field");
+        Label2e.setMinWidth(300);
+        Label2e.setMaxWidth(300);
+        Label2e.setLayoutX(50);
+        Label2e.setLayoutY(450);
+        Label2e.setFont(new Font("Arial",22));
+
+
+        content3.getChildren().addAll(
+                Label2b, Label2c, Label2d, Label2e);
+
+        VBox Entfernung = createSection1("Entfernung");
+        Label Label2f = new Label("Entfernung in m");
+        Label2f.getStyleClass().add("styled-text-field");
+        Label2f.setMinWidth(300);
+        Label2f.setMaxWidth(300);
+        Label2f.setLayoutX(500);
+        Label2f.setLayoutY(450);
+        Label2f.setFont(new Font("Arial",22));
+        content3.getChildren().addAll(
+                Label2f);
+
+        //Informationen zur steuerung
+        String [][] steuerung ={
+                {"W","Vorwärts"},
+                {"S","Rückwärts"},
+                {"A","Links"},
+                {"D","Rechts"},
+                {"→","Turm rechts"},
+                {"←","Turm links"},
+                {"↑","Rohr neigen"},
+                {"↓","Rohr senken"},
+        };
+        for (int i =0;i<steuerung.length;i++){
+            Label taste = new Label(steuerung[i][0]);
+            taste.getStyleClass().add("styled-text-field");
+            taste.setMinWidth(100);
+            taste.setMaxWidth(100);
+            taste.setLayoutX(100+i*150);
+            taste.setLayoutY(850);
+            taste.setFont(new Font("Arial",18));
+
+            Label funktion = new Label(steuerung[i][1]);
+            funktion.getStyleClass().add("styled-text-field");
+            funktion.setMinWidth(100);
+            funktion.setMaxWidth(100);
+            funktion.setLayoutX(100+i*150);
+            funktion.setLayoutY(900);
+            funktion.setFont(new Font("Arial",14));
+
+            content3.getChildren().addAll(taste, funktion);
+        }
 
     }
 
--- src/main/java/com/CUTECAT/app/CUTECAT.java
+++ src/main/java/com/CUTECAT/app/CUTECAT.java
@@ -4,14 +4,11 @@ import com.CUTECAT.GUI.AndreasGUI;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.GridPane;
-import javafx.geometry.Pos;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.layout.Region;
 
@@ -21,6 +18,7 @@ public class CUTECAT extends Application {
     private double xOffset = 200;
     private double yOffset = 0;
     public static boolean Handlung = false;
+    public static boolean Handlung2 = false;
 
     @Override
     public void start(Stage primaryStage) {
@@ -66,6 +64,13 @@ public class CUTECAT extends Application {
         scene.setOnMouseClicked(e ->{
             if (Handlung) {
                 opensecondStage();
+                primaryStage.close();
+            }
+        });
+
+        scene.setOnMouseClicked(e ->{
+            if (Handlung2) {
+                openthirdStage();
                 primaryStage.close();
             }
         });
@@ -159,6 +164,50 @@ public class CUTECAT extends Application {
         setRoundedCorners(root2, 10, 10);
 
         Stage2.show();
+    }
+
+    private void openthirdStage() {
+        //Erzeugen von Stage 3(Modus 2)
+        Stage Stage3 = new Stage();
+
+        // Create 2. main container
+        VBox root3 = new VBox();
+        root3.getStyleClass().add("main-container");
+
+        // Add custom title bar
+        CustomTitleBar titleBar3 = new CustomTitleBar(Stage3);
+
+
+        // Create content area
+        Pane content3 = new Pane();
+        //content3.setHgap(10);
+        //content3.setVgap(10);
+        content3.setPadding(new Insets(20));
+        content3.getStyleClass().add("content-area");
+
+        // Add widgets
+        AndreasGUI.addWidgets3(content3);
+
+        // Add components to root2
+        root3.getChildren().addAll(titleBar3, content3);
+
+        // Create scene
+        Scene scene3 = new Scene(root3,300,300);
+        scene3.setFill(Color.TRANSPARENT);
+        scene3.getStylesheets().add(getClass().getResource("/styles/dark-theme.css").toExternalForm());
+
+        // Configure stage2
+        Stage3.initStyle(StageStyle.TRANSPARENT);
+        Stage3.setScene(scene3);
+        Stage3.setTitle("CUTECAT");
+        //Stage2.setFullScreen(true);
+        Stage3.setHeight(1080);
+        Stage3.setWidth(1920);
+
+        // Apply rounded corners
+        setRoundedCorners(root3, 10, 10);
+
+        Stage3.show();
     }
 
 
