b86601fabdedbfdc186364cb295c07bdc71e025a
DiegoLikesPizza <127695789+DiegoLikesPizza@users.noreply.github.com>
2025-05-22 11:50:57 +0200

    Rename and enhance SemiMode to SemiAutoMode with capabilities
    Renamed SemiMode to SemiAutoMode and implemented multiple capabilities such as movement, shooting, and targeting by adding appropriate methods. This refactoring improves functionality and aligns with the intended purpose of the class.

---
+++ src/main/java/com/CUTECAT/modes/SemiAutoMode.java
@@ -0,0 +1,39 @@
+// SemiAutoMode.java
+package com.CUTECAT.modes;
+
+import com.CUTECAT.modes.capabilities.*;
+
+public class SemiAutoMode extends modebase 
+    implements MovementCapable, ShootingCapable, TargetingCapable {
+
+    @Override
+    public void move(int speed, int direction) {
+
+    }
+
+    @Override
+    public boolean canShoot() {
+        return false;
+    }
+
+    @Override
+    public void aim(int distance, int angle) {
+
+    }
+
+    @Override
+    public boolean isTargetLocked() {
+        return false;
+    }
+
+    @Override
+    protected void handleTargeting() {
+
+    }
+
+    @Override
+    protected void handleMovement() {
+
+    }
+    // Implement all interface methods
+}
