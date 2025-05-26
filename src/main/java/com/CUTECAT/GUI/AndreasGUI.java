872e1556d4a8288969f65205389ed5d42de46683
mayera10 <andreasmayer1003@gmail.com>
2025-05-26 11:06:22 +0200

    Refactor: Widgets to AndreasGUI
    feat: Hauptfenster
    feat: Modi Wahl
    
    feat: Button styling
    feat: button action (noch nicht fertig)
    
    feat: Modus 1
    feat: Modus 1 angefangen
    feat: Anordnung Buttons, etc.
    
    feat: steuerung
    feat: Modus 2 (Halbautomatischer Modus)
    feat: Styling von Headline und Button
    
    feat: Wertangaben bei Parametern in Modus 1 und Modus 2

--- src/main/java/com/CUTECAT/GUI/AndreasGUI.java
+++ src/main/java/com/CUTECAT/GUI/AndreasGUI.java
@@ -6,13 +6,25 @@ import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.layout.Pane;
 
-;
-
 public class AndreasGUI {
 
 
 
     public static void addWidgets(Pane content){
+        TabPane tabPane = new TabPane();
+        tabPane.setLayoutX(0);
+        tabPane.setLayoutY(20);
+        tabPane.getStyleClass().add("styled-tab-pane");
+        Tab tab1 = new Tab("Home");
+
+        Tab tab2 = new Tab("Ferngesteuerter Modus", new Label("Ferngesteuerter Modus"));
+        // Closeable tabs (default)
+        tab1.setClosable(true);
+        tab2.setClosable(true);
+        tabPane.getStyleClass().add("visible-tab-close-button");
+        tabPane.getTabs().addAll(tab1, tab2);
+        content.getChildren().addAll(tabPane);
+
         //Headline links
         VBox HeadlControls = createSection("Headline links");
 
@@ -20,7 +32,7 @@ public class AndreasGUI {
         Label1a.getStyleClass().add("styled-titel");
         Label1a.setMinWidth(1300);
         Label1a.setMaxWidth(1300);
-        Label1a.setFont(new Font("Arial",28));
+        Label1a.setFont(new Font("Arial",40));
         Label1a.setLayoutX(0);
         Label1a.setLayoutY(50);
 
@@ -37,7 +49,7 @@ public class AndreasGUI {
         Label1b.setMaxWidth(650);
         Label1b.setLayoutX(1300);
         Label1b.setLayoutY(50);
-        Label1b.setFont(new Font("Arial",28));
+        Label1b.setFont(new Font("Arial",40));
 
         content.getChildren().addAll(
                 Label1b);
@@ -61,7 +73,7 @@ public class AndreasGUI {
         b2.setLayoutX(1200);
         b2.setLayoutY(400);
         //Button aktivieren
-        b2.setOnAction(e -> CUTECAT.Handlung2=true);
+        b2.setOnAction(e2 -> CUTECAT.Handlung2=true);
 
 
         Button b3 = new Button("Modus 3");
@@ -70,6 +82,7 @@ public class AndreasGUI {
         b3.setPrefSize(600,150);
         b3.setLayoutX(1200);
         b3.setLayoutY(600);
+
 
 
         content.getChildren().addAll(
@@ -87,6 +100,8 @@ public class AndreasGUI {
         b4.setPrefSize(200,100);
         b4.setLayoutX(200);
         b4.setLayoutY(850);
+
+        //b4.setOnAction();
 
         Button b5 = new Button("Impressum");
         //Style button
@@ -142,6 +157,16 @@ public class AndreasGUI {
         Label2b.setLayoutY(150);
         Label2b.setFont(new Font("Arial",22));
 
+        //Wertangabe
+        Label Label2b2 = new Label();
+        Label2b2.setText("30");
+        Label2b2.getStyleClass().add("styled-text-field");
+        Label2b2.setMinWidth(100);
+        Label2b2.setMaxWidth(100);
+        Label2b2.setLayoutX(350);
+        Label2b2.setLayoutY(150);
+        Label2b2.setFont(new Font("Arial",22));
+
 
         //Neigung Rohr
         Label Label2c = new Label("Neigung Rohr");
@@ -151,6 +176,16 @@ public class AndreasGUI {
         Label2c.setLayoutX(50);
         Label2c.setLayoutY(250);
         Label2c.setFont(new Font("Arial",22));
+
+        //Wertangabe
+        Label Label2c2 = new Label();
+        Label2c2.setText("30");
+        Label2c2.getStyleClass().add("styled-text-field");
+        Label2c2.setMinWidth(100);
+        Label2c2.setMaxWidth(100);
+        Label2c2.setLayoutX(350);
+        Label2c2.setLayoutY(250);
+        Label2c2.setFont(new Font("Arial",22));
 
 
         //Munition
@@ -162,6 +197,16 @@ public class AndreasGUI {
         Label2d.setLayoutY(350);
         Label2d.setFont(new Font("Arial",22));
 
+        //Wertangabe
+        Label Label2d2 = new Label();
+        Label2d2.setText("30");
+        Label2d2.getStyleClass().add("styled-text-field");
+        Label2d2.setMinWidth(100);
+        Label2d2.setMaxWidth(100);
+        Label2d2.setLayoutX(350);
+        Label2d2.setLayoutY(350);
+        Label2d2.setFont(new Font("Arial",22));
+
 
         //Gasdruck
         Label Label2e = new Label("Gasdruck");
@@ -172,16 +217,25 @@ public class AndreasGUI {
         Label2e.setLayoutY(450);
         Label2e.setFont(new Font("Arial",22));
 
+        //Wertangabe
+        Label Label2e2 = new Label();
+        Label2e2.setText("30");
+        Label2e2.getStyleClass().add("styled-text-field");
+        Label2e2.setMinWidth(100);
+        Label2e2.setMaxWidth(100);
+        Label2e2.setLayoutX(350);
+        Label2e2.setLayoutY(450);
+        Label2e2.setFont(new Font("Arial",22));
+
 
         content2.getChildren().addAll(
-                Label2b, Label2c, Label2d, Label2e);
+                Label2b, Label2b2, Label2c, Label2c2, Label2d, Label2d2, Label2e, Label2e2);
 
-        VBox Entfernung = createSection1("Entfernung");
         Label Label2f = new Label("Entfernung in m");
         Label2f.getStyleClass().add("styled-text-field");
         Label2f.setMinWidth(300);
         Label2f.setMaxWidth(300);
-        Label2f.setLayoutX(500);
+        Label2f.setLayoutX(600);
         Label2f.setLayoutY(450);
         Label2f.setFont(new Font("Arial",22));
         content2.getChildren().addAll(
@@ -237,58 +291,98 @@ public class AndreasGUI {
         //Anzeige von verschiedenen Parametern
 
         //Rotation Turm
-        Label Label2b = new Label("Rotation Turm");
-        Label2b.getStyleClass().add("styled-text-field");
-        Label2b.setMinWidth(300);
-        Label2b.setMaxWidth(300);
-        Label2b.setLayoutX(50);
-        Label2b.setLayoutY(150);
-        Label2b.setFont(new Font("Arial",22));
+        Label Label3b = new Label("Rotation Turm");
+        Label3b.getStyleClass().add("styled-text-field");
+        Label3b.setMinWidth(300);
+        Label3b.setMaxWidth(300);
+        Label3b.setLayoutX(50);
+        Label3b.setLayoutY(150);
+        Label3b.setFont(new Font("Arial",22));
+
+        //Wertangabe
+        Label Label3b2 = new Label();
+        Label3b2.setText("30");
+        Label3b2.getStyleClass().add("styled-text-field");
+        Label3b2.setMinWidth(100);
+        Label3b2.setMaxWidth(100);
+        Label3b2.setLayoutX(350);
+        Label3b2.setLayoutY(150);
+        Label3b2.setFont(new Font("Arial",22));
 
 
         //Neigung Rohr
-        Label Label2c = new Label("Neigung Rohr");
-        Label2c.getStyleClass().add("styled-text-field");
-        Label2c.setMinWidth(300);
-        Label2c.setMaxWidth(300);
-        Label2c.setLayoutX(50);
-        Label2c.setLayoutY(250);
-        Label2c.setFont(new Font("Arial",22));
+        Label Label3c = new Label("Neigung Rohr");
+        Label3c.getStyleClass().add("styled-text-field");
+        Label3c.setMinWidth(300);
+        Label3c.setMaxWidth(300);
+        Label3c.setLayoutX(50);
+        Label3c.setLayoutY(250);
+        Label3c.setFont(new Font("Arial",22));
+
+        //Wertangabe
+        Label Label3c2 = new Label();
+        Label3c2.setText("30");
+        Label3c2.getStyleClass().add("styled-text-field");
+        Label3c2.setMinWidth(100);
+        Label3c2.setMaxWidth(100);
+        Label3c2.setLayoutX(350);
+        Label3c2.setLayoutY(250);
+        Label3c2.setFont(new Font("Arial",22));
 
 
         //Munition
-        Label Label2d = new Label("Munition");
-        Label2d.getStyleClass().add("styled-text-field");
-        Label2d.setMinWidth(300);
-        Label2d.setMaxWidth(300);
-        Label2d.setLayoutX(50);
-        Label2d.setLayoutY(350);
-        Label2d.setFont(new Font("Arial",22));
+        Label Label3d = new Label("Munition");
+        Label3d.getStyleClass().add("styled-text-field");
+        Label3d.setMinWidth(300);
+        Label3d.setMaxWidth(300);
+        Label3d.setLayoutX(50);
+        Label3d.setLayoutY(350);
+        Label3d.setFont(new Font("Arial",22));
+
+        //Wertangabe
+        Label Label3d2 = new Label();
+        Label3d2.setText("30");
+        Label3d2.getStyleClass().add("styled-text-field");
+        Label3d2.setMinWidth(100);
+        Label3d2.setMaxWidth(100);
+        Label3d2.setLayoutX(350);
+        Label3d2.setLayoutY(350);
+        Label3d2.setFont(new Font("Arial",22));
 
 
         //Gasdruck
-        Label Label2e = new Label("Gasdruck");
-        Label2e.getStyleClass().add("styled-text-field");
-        Label2e.setMinWidth(300);
-        Label2e.setMaxWidth(300);
-        Label2e.setLayoutX(50);
-        Label2e.setLayoutY(450);
-        Label2e.setFont(new Font("Arial",22));
+        Label Label3e = new Label("Gasdruck");
+        Label3e.getStyleClass().add("styled-text-field");
+        Label3e.setMinWidth(300);
+        Label3e.setMaxWidth(300);
+        Label3e.setLayoutX(50);
+        Label3e.setLayoutY(450);
+        Label3e.setFont(new Font("Arial",22));
+
+        //Wertangabe
+        Label Label3e2 = new Label();
+        Label3e2.setText("30");
+        Label3e2.getStyleClass().add("styled-text-field");
+        Label3e2.setMinWidth(100);
+        Label3e2.setMaxWidth(100);
+        Label3e2.setLayoutX(350);
+        Label3e2.setLayoutY(450);
+        Label3e2.setFont(new Font("Arial",22));
 
 
         content3.getChildren().addAll(
-                Label2b, Label2c, Label2d, Label2e);
+                Label3b, Label3b2, Label3c, Label3c2, Label3d, Label3d2, Label3e, Label3e2);
 
         VBox Entfernung = createSection1("Entfernung");
-        Label Label2f = new Label("Entfernung in m");
-        Label2f.getStyleClass().add("styled-text-field");
-        Label2f.setMinWidth(300);
-        Label2f.setMaxWidth(300);
-        Label2f.setLayoutX(500);
-        Label2f.setLayoutY(450);
-        Label2f.setFont(new Font("Arial",22));
+        Label Label3f = new Label("Entfernung in m");
+        Label3f.getStyleClass().add("styled-text-field");
+        Label3f.setMinWidth(300);
+        Label3f.setMaxWidth(300);
+        Label3f.setLayoutX(600);
+        Label3f.setLayoutY(450);
+        Label3f.setFont(new Font("Arial",22));
         content3.getChildren().addAll(
-                Label2f);
+                Label3f);
 
         //Informationen zur steuerung
         String [][] steuerung ={
@@ -320,6 +414,16 @@ public class AndreasGUI {
 
             content3.getChildren().addAll(taste, funktion);
         }
+
+        Button Abschluss = new Button("Abschluss");
+        Abschluss.getStyleClass().add("styled-text-field");
+        Abschluss.setMinWidth(300);
+        Abschluss.setMaxWidth(300);
+        Abschluss.setLayoutX(1400);
+        Abschluss.setLayoutY(650);
+        Abschluss.setFont(new Font("Arial",22));
+        content3.getChildren().addAll(Abschluss);
+
 
     }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module CUTECAT {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires java.desktop;
 
     opens com.CUTECAT.app to javafx.fxml;
     exports com.CUTECAT.diegoutil;
